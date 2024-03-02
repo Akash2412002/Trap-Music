f1381e5bc2271c73f695b552b451b6ff46a09629
Akash2412002 <akash2002avt@gmail.com>
2024-03-02 19:04:20 +0530

    project initial commit

--- Trap_Music/src/main/java/com/trap_music/TrapMusicApplication.java
+++ Trap_Music/src/main/java/com/trap_music/TrapMusicApplication.java
@@ -3,7 +3,7 @@ package com.trap_music;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class TrapMusicApplication {
 
 	public static void main(String[] args) {
