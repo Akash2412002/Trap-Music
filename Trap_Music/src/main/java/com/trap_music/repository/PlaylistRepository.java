a612fbace429b8bda575edb561fe038baa6ff90f
Akash M <96969816+Akash2412002@users.noreply.github.com>
2024-03-02 22:54:49 +0530

    Delete Trap_Music directory

--- Trap_Music/src/main/java/com/trap_music/repository/PlaylistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trap_music.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.trap_music.entity.Playlist;
-
-public interface PlaylistRepository extends JpaRepository<Playlist, Integer>{
-
-}
