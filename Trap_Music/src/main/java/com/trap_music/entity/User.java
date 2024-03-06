4253bdcd495ae9bb03e9dd70f24fb19a10b805fc
Akash2412002 <akash2002avt@gmail.com>
2024-03-06 14:32:40 +0530

    Final Commit

--- Trap_Music/src/main/java/com/trap_music/entity/User.java
+++ Trap_Music/src/main/java/com/trap_music/entity/User.java
@@ -1,15 +1,9 @@
 package com.trap_music.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 
 @Entity
 public class User {
